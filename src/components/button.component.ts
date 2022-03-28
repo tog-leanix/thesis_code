48c2b09f01d6e95718ea7808347798e28c42b5ff
Tom Gehder <tom.gehder@leanix.net>
2022-03-28 12:00:47 +0200

    Build and use atoms  in lib

--- src/components/button.component.ts
+++ src/components/button.component.ts
@@ -1,7 +1,15 @@
-import { LitElement, html, customElement, property } from "lit-element";
-
+import { LitElement, html, css, customElement, property } from "lit-element";
 @customElement("saas-button")
 class ButtonComponent extends LitElement {
+  static styles = css`
+    button {
+      padding: var(--md-spacing) var(--lg-spacing);
+      color: var(--text-color);
+      background: var(--primary-color, #ccc);
+      border: none;
+      border-radius: 5px;
+    }
+  `;
   @property() myLabel: string = "";
   render() {
     return html`<button>${this.myLabel}</button>`;

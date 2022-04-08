0a934d215c902db914004f7ed15a91489328e1b4
Tom Gehder <tom.gehder@leanix.net>
2022-04-08 14:18:14 +0200

    Add table story with more data

--- src/components/table.component.ts
+++ src/components/table.component.ts
@@ -1,27 +1,75 @@
-import { customElement, LitElement, html, property } from "lit-element";
+import { customElement, LitElement, html, property, css } from "lit-element";
 import { PREFIX } from "../constants";
+
+interface TableHeader {
+  [key: string]: string;
+}
+
 @customElement(PREFIX + "table")
 class TableComponent extends LitElement {
+  static styles = css`
+    table {
+      color: var(--text-color);
+      width: 100%;
+      border-collapse: collapse;
+    }
+
+    tr {
+      height: var(--table-row-height);
+    }
+
+    thead {
+      color: var(--text-color);
+      background: var(--primary-color);
+      border-bottom: solid 2px var(--grey-90);
+    }
+
+    tbody > tr {
+      border-bottom: solid 1px var(--grey-30);
+    }
+
+    tbody > tr:nth-child(2n + 1) {
+      background: var(--grey-90);
+    }
+
+    td {
+      padding: var(--sm-spacing) var(--md-spacing);
+    }
+  `;
+  @property({ type: Object }) header: TableHeader = {};
   @property({ type: Array }) data: any[] = [];
   render() {
     return html`
       <table>
-        <thead></thead>
+        <thead>
+          <tr>
+            ${this.header ? this.renderHeader(this.header) : null}
+          </tr>
+        </thead>
         <tbody>
-          ${this.data ? this.data.map(this.renderRow) : ""}
-          <tr></tr>
+          ${this.data
+            ? this.data.map((d) => this.renderRow(d, this.header))
+            : ""}
         </tbody>
       </table>
     `;
   }
 
-  renderRow(data: Record<string, string | number> | string | number) {
+  renderHeader(header: TableHeader) {
+    const headerLabels = Object.values(header);
+    return headerLabels.map((label) => html`<th>${label}</th>`);
+  }
+
+  renderRow(
+    data: Record<string, string | number> | string | number,
+    header: TableHeader
+  ) {
     if (typeof data !== "object") {
       return html`<tr>
         <td>${data}</td>
       </tr>`;
     } else {
-      const keys = Object.keys(data);
+      const keys = Object.keys(header);
       return html`<tr>
         ${keys.map((key) => {
           const value = data[key];
